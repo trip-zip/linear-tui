b2d31d1f639b0e4d92e5591c89be0d05bfdd1f63
Jimmy Cozza <moatcozza@gmail.com>
2025-06-06 11:05:28 -0600

    restore

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Warning: Could not load .env file: %v", err)
+	}
+
+	// Check if we have the required API key
+	if os.Getenv("LINEAR_API_KEY") == "" {
+		fmt.Println("Error: LINEAR_API_KEY environment variable not set")
+		fmt.Println("Please create a .env file with your Linear API key:")
+		fmt.Println("LINEAR_API_KEY=your_api_key_here")
+		os.Exit(1)
+	}
+
+	// Initialize and execute CLI
+	initCLI()
+	executeCLI()
+}
+
